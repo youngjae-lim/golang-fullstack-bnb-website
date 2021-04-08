06190df4fa745409c79bc8ef9dee81a50387b9e6
Youngjae <limyoungjae@gmail.com>
2021-04-07 23:25:46 -0400

    Added functionality of sending and processing an AJAX request

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
 	"github.com/youngjae-lim/golang-fullstack-bnb-website/pkg/config"
 	"github.com/youngjae-lim/golang-fullstack-bnb-website/pkg/models"
 )
@@ -21,12 +22,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTempalte renders html templates
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache { // if prod mode
@@ -44,7 +46,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
