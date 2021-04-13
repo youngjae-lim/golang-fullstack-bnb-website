1bd6c6ccad0ba14e2dc56ef1161769e10b98cde9
Youngjae <limyoungjae@gmail.com>
2021-04-12 21:31:20 -0400

    added a migration for seeding restrictions table

---
+++ migrations/20210413012946_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2021-04-11 00:00:00.000','2021-04-11 00:00:00.000'),
+	 ('Owner Block','2021-06-15 00:00:00.000','2021-06-15 00:00:00.000');
