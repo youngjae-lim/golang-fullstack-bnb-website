977899c8a5327c6221af79a7da197a18aafb92f2
Youngjae <limyoungjae@gmail.com>
2021-04-15 00:50:41 -0400

    added protected routes with authentication middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -40,5 +40,12 @@ func routes(app *config.AppConfig) http.Handler {
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+	// protected routes
+	mux.Route("/admin", func(mux chi.Router){
+		mux.Use(Auth)
+
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+	})
+
 	return mux
 }
