8aa73c0273dcc3c7ac6e5fee376acc643a5a6750
Youngjae <limyoungjae@gmail.com>
2021-04-14 20:45:51 -0400

    added a handler for login page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -292,7 +292,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		From:     "me@here.com",
 		Subject:  "Reservation Confirmation",
 		Content:  htmlMessage,
-		Template: "basic.html", 
+		Template: "basic.html",
 	}
 
 	m.App.MailChan <- msg
@@ -487,4 +487,10 @@ func (m *Repository) BookRoom(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "reservation", res)
 
 	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
+}
+
+func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
 }
