dfada052e18b931399a2fcacf2e4564f52788713
Youngjae <limyoungjae@gmail.com>
2021-04-14 21:36:38 -0400

    created authentication and user database functions

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -104,3 +104,20 @@ func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
 	}
 	return room, nil
 }
+
+// GetUserByID returns a user by id
+func (m *testDBRepo) GetUserByID(id int) (models.User, error) {
+	var u models.User
+
+	return u, nil
+}
+
+// UpdateUser updates user info in the users table
+func (m *testDBRepo) UpdateUser(u models.User) error {
+	return nil
+}
+
+// Authenticate authenticates a user
+func (m *testDBRepo) Authenticate(email, unhashedPassword string) (int, string, error) {
+	return 1, "", nil
+}
